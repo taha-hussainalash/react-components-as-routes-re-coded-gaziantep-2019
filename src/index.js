666a5e1f5c5b67b676f5989ca62e3fe1eb989865
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 10:41:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -33,12 +33,13 @@ const Login = () => {
     </div>
   );
 };
-
 ReactDOM.render((
   <Router>
+    <React.Fragment>
       <Route path="/" render={Home} />
       <Route exact path="/about" render={About} />
       <Route exact path="/login" render={Login} />
+    </React.Fragment>
   </Router>),
   document.getElementById('root')
 );

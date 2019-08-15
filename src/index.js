65ce056ffe5b50439433f4bded2a26383acf312d
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 10:38:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -16,7 +16,23 @@ const About = () => {
   );
 };
 
-
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 
 ReactDOM.render(
  (

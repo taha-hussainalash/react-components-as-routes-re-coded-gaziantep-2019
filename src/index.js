c70ba24a19caef3e0b9d92ca355b578e672c428f
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 10:43:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    )
+  }
+}
+
 const About = () => {
   return (
     <div>

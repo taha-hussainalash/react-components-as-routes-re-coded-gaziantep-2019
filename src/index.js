d8ae23e587a75aca00c69000a83a9c9fd214c02b
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 10:46:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -14,50 +14,101 @@ const link = {
 const Navbar = () =>
   <div>
     <NavLink
-      to="/" exact style={link}
-
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Home!</h1>
-      </div>
-    )
-  }
-}
-
-const About = () => {
-  return (
+      to="/" exact style={link} 
+       activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+ 
+const Home = () => <h1>Home!</h1>;
+ 
+const About = () => <h1>This is my about component!</h1>;
+ 
+const Login = () =>
+  <form>
+    <h1>Login</h1>
     <div>
-      <h1>This is my about component!</h1>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
     </div>
-  );
-};
-
-const Login = () => {
-  return (
     <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
     </div>
-  );
-};
+    <input type="submit" value="Login" />
+  </form>;
+ 
 ReactDOM.render((
   <Router>
     <React.Fragment>
-      <Route path="/" render={Home} />
-      <Route exact path="/about" render={About} />
-      <Route exact path="/login" render={Login} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </React.Fragment>
   </Router>),
   document.getElementById('root')
 );
+
+// class Home extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Home!</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+// ReactDOM.render((
+//   <Router>
+//     <React.Fragment>
+//       <Route path="/" render={Home} />
+//       <Route exact path="/about" render={About} />
+//       <Route exact path="/login" render={Login} />
+//     </React.Fragment>
+//   </Router>),
+//   document.getElementById('root')
+// );
